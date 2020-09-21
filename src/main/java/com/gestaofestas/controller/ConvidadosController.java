d3aed8dd01345b85ee6f638b315372e231cca505
Everton__Levi <everton.levi@hotmail.com>
2020-09-21 02:31:54 -0300

    Test Thymeleaf

--- src/main/java/com/gestaofestas/controller/ConvidadosController.java
+++ src/main/java/com/gestaofestas/controller/ConvidadosController.java
@@ -11,12 +11,18 @@ import org.springframework.web.servlet.ModelAndView;
 public class ConvidadosController {
 	@Autowired
 	private Convidados convidados;
+
 	@GetMapping("/convidados")
 	public ModelAndView listar() {
-		ModelAndView modelAndView = new ModelAndView("ListaConvidados");
+		ModelAndView modelAndView = new ModelAndView("lista-convidados");
 		modelAndView.addObject("convidados", convidados.findAll());
 
 		System.out.println("***** " + convidados.findAll().toString() + " *******");
 		return modelAndView;
+	}
+
+	@GetMapping("/hello")
+	public String hello() {
+		return "hello";
 	}
 }
